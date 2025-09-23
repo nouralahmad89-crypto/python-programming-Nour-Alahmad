37bf131c0bdacbca984c0c039fc91195d2509f5e
Nour <nouralahmad89@gmail.com>
2025-09-23 11:46:29 +0200

    done

---
+++ Exercises/scatter.py
@@ -0,0 +1,28 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+x= np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y= np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.scatter(x,y)
+#plt.show()
+# set your own colour
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.scatter(x, y, color = 'hotpink')
+
+x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+plt.scatter(x, y, color = '#88c999')
+#plt.show()
+#specify the colormap with the keyword argument cmap
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+plt.scatter(x, y, c=colors, cmap='viridis')
+#plt.show()
+# change the size of the dots with the s argument
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+plt.scatter(x, y, s=sizes)
+plt.show()
